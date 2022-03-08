cac6548f9d6490bfb7e7be92f2faa114a3b5902d
krinal-19 <98145592+krinal-19@users.noreply.github.com>
2022-03-08 23:58:23 +0530

    Add files via upload

---
+++ a1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+
+int main(){
+	int fd1,fd2;
+	char readbuf[50];
+	fd1=open("a1r.txt",O_RDONLY,777);
+	fd2=open("a1w.txt",O_CREAT|O_RDWR,777);
+	
+	while((read(fd1,readbuf,50))>0){}
+	printf("data from buffer: %s\n",readbuf);
+	
+	write(fd2,readbuf,300);
+	close(fd1);
+	close(fd2);
+	return 0;		
+}
